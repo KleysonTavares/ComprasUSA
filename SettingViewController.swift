68b15e7bc0e477317fcbd22a8da88d86b0a4090c
Kleyson Tavares <kleyson.tavares@icloud.com>
2020-10-18 10:42:42 -0300

    atualização das constraint

---
+++ SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  comprasUSA
+//
+//  Created by Kleyson on 21/09/2020.
+//  Copyright © 2020 Kleyson. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ comprasUSA/SettingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.swift
+//  comprasUSA
+//
+//  Created by Kleyson on 21/09/2020.
+//  Copyright © 2020 Kleyson. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: NSObject {
+
+}
